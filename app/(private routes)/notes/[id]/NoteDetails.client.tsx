87b809eeb14da84f4fb0f27bbbf3136b47adf4e9
Eva <evatereshenko12321@gmail.com>
2025-09-21 20:43:14 +0300

    hw

--- app/(private routes)/notes/[id]/NoteDetails.client.tsx
+++ app/(private routes)/notes/[id]/NoteDetails.client.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { fetchNoteById } from "@/lib/api/api";
+import { fetchNoteById } from "@/lib/api/clientApi";
 import { useQuery } from "@tanstack/react-query";
 
 import css from "./NoteDetails.client.module.css";
